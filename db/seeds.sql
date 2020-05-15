490a7eeea8235eb2da92423f83918b0e014a169a
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-05-15 00:15:11 -0400

    changed burgers to cats, did not change code. sry

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 --  insert queries to populate the burgers table with at least three entries.
-INSERT INTO burgers (name, devoured) VALUES ("Cheeseburger", "0");
-INSERT INTO burgers (name, devoured) VALUES ("Double Cheeseburger", "1");
-INSERT INTO burgers (name, devoured) VALUES ("Chicken Sandwich", "0");
+INSERT INTO burgers (name, devoured) VALUES ("Maine Coon", "0");
+INSERT INTO burgers (name, devoured) VALUES ("Tabby", "1");
+INSERT INTO burgers (name, devoured) VALUES ("Siamese", "0");
 
 -- Run the schema.sql and seeds.sql files into the mysql server from the command line
