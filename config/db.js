66ab41df317dc1d7a4fd61899de091c460e85bbc
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-05-18 00:02:51 -0400

    setting up heroku with cleardb

--- config/db.js
+++ config/db.js
@@ -3,15 +3,14 @@ module.exports = {
         host: "localhost",
         port: 3306,
         user: "root",
-        //adding process.env so I don't have to show my password in my code
         password: process.env.PASSWORD,
         database: "burgersDB"
     },
     heroku: {
-        host: "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "us-cdbr-east-06.cleardb.net",
         port: 3306,
-        user: "iuu3k2kco9mjowpp",
-        password: "DB_PASSWORD",
-        database: "t4a1t4a2eeysw4u2",
+        user: "b7b8a95dceda29",
+        password: process.env.DB_PASSWORD,
+        database: "heroku_44410e69722b71e",
     }
 }
