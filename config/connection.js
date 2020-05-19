4413b4e8138cc596154424355f6341b426407851
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-05-18 21:10:32 -0400

    fixing heroku

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,14 @@ var mysql = require("mysql");
 const config = require("./db");
 
 const dbCreds = (process.env.NODE_ENV === "production") ? config.heroku : config.db;
-const connection = mysql.createPool(dbCreds); +
-    connection.on('error', connectDb());
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+const connection = mysql.createPool(dbCreds);
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
 // Export the connection.
 module.exports = connection;
